1d8e0485e5a2879c9b5b2e967188a82c008a18e2
BecoolGhub <leninkumar.official@gmail.com>
2025-09-04 16:08:07 +0530

    adding new project file - BattleOne

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+
+import Course from './course';
+import  html from './assets/html.png'
+import css from './assets/css.png'
+import js from './assets/js.png'
+import bs from './assets/bs.jpeg'
+
+
+function App() {
+  return (
+    <>
+    <Course name="HTML" price="Rs.200" buy="Buy Course" image={html} rating="Rating"/>
+    <Course name="CSS" price="Rs.300" buy="Buy Course" image={css} rating="Rating"/>
+    <Course name="JAVASCRIPT" price="Rs.400" buy="Buy Course" image={js} rating="Rating"/>
+    <Course name="BOOTSTRAP" price="Rs.400" buy="Buy Course" image={bs} rating="Rating"/>
+    
+    </>
+  );
+}
+
+export default App;
---
+++ src/course.jsx
@@ -0,0 +1,18 @@
+
+function Course(props) {
+
+    return (
+        <nav className="card">
+            <img src={props.image} className='image' />
+            <h3 >{props.name}</h3>
+            <p>{props.price}</p>
+            <div className="bottom">
+                <button>{props.buy}</button>
+                <p className="rat">{props.rating}</p>
+            </div>
+
+        </nav>
+    );
+}
+
+export default Course;

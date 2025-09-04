5d84f5bc32616e607977da8baad84dcee84711cd
LENIN KUMAR <leninkumar.official@gmail.com>
2025-09-04 16:52:18 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -22,6 +22,19 @@ function App() {
     <Course name="BOOTSTRAP" price="Rs.400" buy="Buy Course" image={bs} rating="Rating"/>
     <Course name="BOOTSTRAP" price="Rs.400" buy="Buy Course" image={bs} rating="Rating"/>
 
+      <Course name="HTML" price="Rs.200" buy="Buy Course" image={html} rating="Rating"/>
+    <Course name="CSS" price="Rs.300" buy="Buy Course" image={css} rating="Rating"/>
+    <Course name="HTML" price="Rs.200" buy="Buy Course" image={html} rating="Rating"/>
+    <Course name="CSS" price="Rs.300" buy="Buy Course" image={css} rating="Rating"/>
+    <Course name="HTML" price="Rs.200" buy="Buy Course" image={html} rating="Rating"/>
+    <Course name="CSS" price="Rs.300" buy="Buy Course" image={css} rating="Rating"/>
+    
+    <Course name="JAVASCRIPT" price="Rs.400" buy="Buy Course" image={js} rating="Rating"/>
+    <Course name="BOOTSTRAP" price="Rs.400" buy="Buy Course" image={bs} rating="Rating"/>
+    <Course name="BOOTSTRAP" price="Rs.400" buy="Buy Course" image={bs} rating="Rating"/>
+    <Course name="BOOTSTRAP" price="Rs.400" buy="Buy Course" image={bs} rating="Rating"/>
+    <Course name="BOOTSTRAP" price="Rs.400" buy="Buy Course" image={bs} rating="Rating"/>
+
     </>
   );
 }

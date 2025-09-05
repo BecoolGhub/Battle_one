67fc6c0b419e94129c25765552aa79f2b3085580
BecoolGhub <leninkumar.official@gmail.com>
2025-09-05 06:49:58 +0530

    changes added

--- src/course.jsx
+++ src/course.jsx
@@ -1,19 +1,24 @@
 
 function Course(props) {
 
-    return (
-    
-        <nav className="card">
+   if(props.show){
+     return (
+
+        <nav className="card" >
             <img src={props.image} className='image' />
             <h3 >{props.name}</h3>
             <p>{props.price}</p>
             <div className="bottom">
                 <button>{props.buy}</button>
-                <p className="rat">{props.rating}</p>
+                <p >{props.rating}</p>
             </div>
-
         </nav>
     );
+   }else{
+    return(
+        <div className="card">course not available</div>
+    );
+   }
 }
 
 export default Course;

7bae77ae759d9be40f36608dfcb298d6ee08a2d2
BecoolGhub <leninkumar.official@gmail.com>
2025-09-09 02:34:33 +0530

    .gitattributes file added

--- src/course.jsx
+++ src/course.jsx
@@ -1,16 +1,25 @@
+import { useEffect, useState } from "react";
 
 function Course(props) {
 
+    const [purchased , setPurchased] = useState(false);
+    const [discount , setDiscount] = useState(props.price);
+
+    function Buycourse(amount) {
+        console.log(props.name, "purchased", discount, "% discount");
+        setPurchased(true);
+        setDiscount(discount - amount);
+    }
+
     return (
 
-      props.name &&  <nav className="card" >
+        props.name && <nav className="card" >
             <img src={props.image} className='image' />
             <h3 >{props.name}</h3>
-            <p>{props.price}</p>
-            <div className="bottom">
-                <button>{props.buy}</button>
-                <p >{props.rating}</p>
-            </div>
+            <p>{discount}</p>
+            <button onClick={(event) => Buycourse(20)}>buy now</button>
+            <button onClick={() => props.delete(props.id)}>DELETE</button>
+            <p>{ purchased ? "course purchased..." : "Get it now"}</p>
         </nav>
     );
 
--- src/courselist.jsx
+++ src/courselist.jsx
@@ -4,71 +4,54 @@ import css from './assets/css.png'
 import js from './assets/js.png'
 import bs from './assets/bs.jpeg'
 import react from './assets/js.png'
+import { useState } from 'react';
+import { useEffect } from 'react';
 
 function CourseList() {
 
-    const courses = [
-        {
-            id : 1,
-            name: "HTML",
-            price: 199,
-            buy: "Buy Course",
-            image: html,
-            rating: 5
-        },
-        {
-            id : 2,
-            name: "CSS",
-            price: 299,
-            buy: "Buy Course",
-            image: css,
-            rating: 5
-        },
-        {
-            id : 3,
-            name: "JS",
-            price: 399,
-            buy: "Buy Course",
-            image: js,
-            rating: 5
-        },
-        {
-            id : 4,
-            name: "BS",
-            price: 350,
-            buy: "Buy Course",
-            image: bs,
-            rating: 5
-        },
-        {
-            id : 5,
-            name: "REACT",
-            price: 1000,
-            buy: "Buy Course",
-            image: react,
-            rating: 5
-        },
-    ]
+    const [courses, setCourses] = useState(null);
 
-    courses.sort((x,y) => y.price - x.price)
+    const [dummy , setDummy] = useState(true);
+    const [dummy1, setDummy1] = useState(false);
 
-    // const bgcourse = courses.filter((course) => course.price < 200)
+    useEffect(() => {
+        fetch('http://localhost:3000/courses')
+        .then(response => {
+            console.log(response);
+            return response.json()
+        })
+        .then(data => setCourses(data))
+    }, []);
+
+   
+
+    function handledelete(id) {
+        const newCourses = courses.filter((course) => course.id < id);
+        setCourses(newCourses);
+    }
+
+
+    if(!courses){
+        return <></>
+    }
 
     const courselist = courses.map(
         (course) =>
             <Course
                 key={course.id}
-        name={course.name}
+                name={course.name}
                 image={course.image}
                 price={course.price}
-                rating={course.rating}
-                buy={course.buy}
+                delete={handledelete}
+                id={course.id}
             />
     )
 
     return (
         <>
             {courselist}
+            <p onClick={() => {setDummy1(true)}}> FUCK</p>
+            <button onClick={ () => {setDummy(false)}}>Dummy button</button>
         </>
     );
 }
